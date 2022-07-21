8151ec9a3a1f109ad9f253cf90505318c54947c2
Will Hanstedt <whanstedt@gmail.com>
2022-07-21 17:47:06 -0400

    Phase diagrams

--- k-basis.py
+++ k-basis.py
@@ -3,23 +3,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.ticker as tck
 
+L = 50
 
-L = 20
-epsilon_0 = 0.3
-m_0 = 1.5
-m_1 = -0.7
-v = 0.1
-delta = 0.1
+def make_h(k_x, k_y, new_values={}):
+    if not new_values:
+        epsilon_0 = 0.5
+        m_0 = -0.8
+        m_1 = -1.4
+        v = 0.3
+        delta = 0.6
+    else:
+        epsilon_0 = new_values["epsilon_0"]
+        m_0 = new_values["m_0"]
+        m_1 = new_values["m_1"]
+        v = new_values["v"]
+        delta = new_values["delta"]
 
-
-def make_h(k_x, k_y):
     h_k = np.zeros((8, 8), dtype=complex)
     h_k += epsilon_0 * kron([paulis[3], paulis[0], paulis[0]])
     h_k += (m_0 + m_1 * (np.cos(k_x) + np.cos(k_y))) * kron([paulis[3], paulis[0], paulis[3]])
     h_k += v * np.sin(k_x) * kron([paulis[0], paulis[3], paulis[1]])
     h_k += v * np.sin(k_y) * kron([paulis[3], paulis[0], paulis[2]])
     h_k += delta * np.sin(k_x) * kron([paulis[1], paulis[3], paulis[0]])
-    # h_k += delta * np.sin(k_y) * kron([paulis[2], paulis[0], paulis[3]])
+    h_k += delta * np.sin(k_y) * kron([paulis[2], paulis[0], paulis[3]])
+    # print("Warning: k_y term is being excluded")
     np.testing.assert_almost_equal(h_k.conj().transpose(), h_k)
     return h_k
 
@@ -33,11 +40,11 @@ def main():
     all_evals = []
     normal_state_only = False  # Only matters when epsilon != 0 or delta != 0
     for n in range(L):
-        for m in range(L):  # TODO: CHANGE BACK
+        # for m in range(L):  # TODO: CHANGE BACK
             k_x = n * delta_k
             if k_x > np.pi:
                 k_x -= 2 * np.pi
-            k_y = m * delta_k  # TODO: CHANGE BACK
+            k_y = 0  # m * delta_k  # TODO: CHANGE BACK
             if k_y > np.pi:
                 k_y -= 2 * np.pi
             h_k = make_h(k_x, k_y)
@@ -55,22 +62,22 @@ def main():
     # plt.plot(np.arange(len(all_evals)), all_evals, "o")
     # plt.show()
 
-    # # Plot bands
-    # plot_points = np.vstack(points)
-    # plt.plot(plot_points[:, 1]/np.pi, plot_points[:, 2], ".")
-    # plt.title("Band structure (%dx%d lattice), $\kappa=%d$" %(L, L, sys_kappa.real))
-    # plt.xlabel("Crystal momentum $k_x$")
-    # plt.ylabel("Energy eigenvalue")
-    # axes = plt.figure(num=1).get_axes()[0]
-    # axes.xaxis.set_major_formatter(tck.FormatStrFormatter('%g $\\frac{\pi}{a}$'))
-    # axes.xaxis.set_major_locator(tck.MultipleLocator(base=1/2))
-    # plt.show()
-
-    # Try to plot 3D
+    # Plot bands
     plot_points = np.vstack(points)
-    axes = plt.axes(projection="3d")
-    axes.scatter(plot_points[:, 0], plot_points[:, 1], plot_points[:, 2])
+    plt.plot(plot_points[:, 0]/np.pi, plot_points[:, 2], ".")
+    plt.title("Band structure (%dx%d lattice), $\kappa=%d$" %(L, L, round(sys_kappa.real)))
+    plt.xlabel("Crystal momentum $k_x$")
+    plt.ylabel("Energy eigenvalue")
+    axes = plt.figure(num=1).get_axes()[0]
+    axes.xaxis.set_major_formatter(tck.FormatStrFormatter('%g $\\frac{\pi}{a}$'))
+    axes.xaxis.set_major_locator(tck.MultipleLocator(base=1/2))
     plt.show()
+
+    # # Try to plot 3D
+    # plot_points = np.vstack(points)
+    # axes = plt.axes(projection="3d")
+    # axes.scatter(plot_points[:, 0], plot_points[:, 1], plot_points[:, 2])
+    # plt.show()
 
     print("Kappa: " + str(sys_kappa))
 
@@ -102,7 +109,7 @@ def dirac_point():
     plt.show()
 
 
-def kappa():
+def kappa(new_values={}):
     inversion = kron([paulis[3], paulis[0], paulis[3]])
     kappa = 0
     # Iterate over the points
@@ -110,7 +117,7 @@ def kappa():
         # print("(%d, %d)" %(x, y))
         k_x = x * np.pi
         k_y = y * np.pi
-        h_k = make_h(k_x, k_y)
+        h_k = make_h(k_x, k_y, new_values=new_values)
 
         vals, vecs = np.linalg.eigh(h_k)
         # indices = np.argsort(vals)
@@ -129,5 +136,28 @@ def kappa():
     return kappa
 
 
+def scan_parameters(epsilon_0, v, delta):
+    m_0s = np.arange(-2, 2, 0.1)
+    m_1s = np.arange(-2, 2, 0.1)
+    m_0s, m_1s = np.meshgrid(m_0s, m_1s)
+    kappas = np.zeros(m_0s.shape, dtype=float)
+    for i in range(m_0s.shape[0]):
+        for j in range(m_0s.shape[1]):
+            kappas[i, j] = kappa(new_values={
+                "epsilon_0": epsilon_0,
+                "m_0": m_0s[i, j],
+                "m_1": m_1s[i, j],
+                "v": v,
+                "delta": delta
+            }).real
+    plt.pcolormesh(m_0s, m_1s, kappas, cmap="Set1")
+    plt.title("$\\varepsilon_0 = %f, v = %f, \Delta = %f$" %(epsilon_0, v, delta))
+    plt.colorbar()
+    plt.xlabel("$m_0$")
+    plt.ylabel("$m_1$")
+    plt.show()
+
+
 if __name__ == "__main__":
     main()
+    # scan_parameters(0.2, 0.9, 0.1)
