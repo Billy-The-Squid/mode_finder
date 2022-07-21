3e1b52815326f73c01534a56a669136c98243086
Will Hanstedt <whanstedt@gmail.com>
2022-07-21 11:41:21 -0400

    Remove old generation methods

--- toy_model.py
+++ toy_model.py
@@ -49,48 +49,6 @@ def setup_system(L, epsilon_0, m_0, m_1, v, delta, verbose=False):
     return latt
 
 
-def setup_system_old(L, epsilon_0, m_0, m_1, v, delta, verbose=False):
-    """
-    Sets up the system and returns the lattice.Lattice
-    :param L:
-    :param epsilon_0:
-    :param m_0:
-    :param m_1:
-    :param v:
-    :param delta:
-    :param verbose:
-    :return:
-    """
-    # Create the matrix
-    total_init = perf_counter()
-    latt = Lattice(L, {"s": ("up", "down"), "rho": ("s", "p-")})
-    if verbose:
-        print("Time to create matrix: " + str(perf_counter() - total_init))
-
-    # Add terms
-    # 1
-    latt.add_diagonal_old(epsilon_0, 3, {"s": 0, "rho": 0}, verbose=verbose)
-    # 2
-    latt.add_diagonal_old(m_0, 3, {"s": 0, "rho": 3}, verbose=verbose)
-    latt.add_hopping_old(0.5 * m_1, 3, {"s": 0, "rho": 3}, 0, verbose=verbose)
-    latt.add_hopping_old(0.5 * m_1, 3, {"s": 0, "rho": 3}, 1, verbose=verbose)
-    # 3
-    latt.add_hopping_old(-1j * v / 2, 0, {"s": 3, "rho": 1}, 0, verbose=verbose)
-    # 4
-    latt.add_hopping_old(-1j * v / 2, 3, {"s": 0, "rho": 2}, 1, verbose=verbose)
-    # 5
-    latt.add_hopping_old(-1j * delta / 2, 1, {"s": 3, "rho": 0}, 0, verbose=verbose)
-    latt.add_hopping_old(1j * delta / 2, 1, {"s": 3, "rho": 0}, 2, verbose=verbose)
-    # 6
-    latt.add_hopping_old(-1j * delta / 2, 2, {"s": 0, "rho": 3}, 1, verbose=verbose)
-    latt.add_hopping_old(1j * delta / 2, 2, {"s": 0, "rho": 3}, 3, verbose=verbose)
-
-    if verbose:
-        print("Time to create entire system: " + str(perf_counter() - total_init))
-
-    return latt
-
-
 def main():
     # Initial parameters
     L = 3
